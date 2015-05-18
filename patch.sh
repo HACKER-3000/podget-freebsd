023af95489567d46f184f2e33cde64a72eaef23e
Andrew Turner <andrewjamesturner0@gmail.com>
2015-05-18 23:04:32 +0100

    Add script to make podget compatible with FreeBSD
    * Change sed to gsed

---
+++ patch.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+PODGET_VERSION=0.7.6
+
+# Dowload and extract podget
+fetch http://downloads.sourceforge.net/project/podget/podget/podget-"$PODGET_VERSION"/podget-"$PODGET_VERSION".tar.gz
+tar -xf podget-"$PODGET_VERSION".tar.gz
+cd podget-"$PODGET_VERSION"
+
+# Change sed to gsed
+sed -i '.orig' 's/sed/gsed/g' podget
