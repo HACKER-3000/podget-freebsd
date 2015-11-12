a237de82ea3dbc7aae57658450fd90eeaa763861
Andrew Turner <andrewjamesturner0@gmail.com>
2015-11-12 22:17:08 +0000

    update install.sh: podget project moved to GitHub

--- install.sh
+++ install.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-PODGET_VERSION=0.7.8
-
 # Dowload and extract podget
-fetch http://downloads.sourceforge.net/project/podget/podget/podget-"$PODGET_VERSION"/podget-"$PODGET_VERSION".tar.gz
-tar -xf podget-"$PODGET_VERSION".tar.gz
-cd podget-"$PODGET_VERSION"
+fetch https://github.com/dvehrs/podget/archive/master.zip
+unzip master.zip
+cd podget-master
 
 # Change sed to gsed
 sed -i '.orig' 's/sed/gsed/g' podget
 
 # Install
 sudo gmake prefix=/usr/local all install
+
+cd -
+rm master.zip
+rm -rf podget-master
